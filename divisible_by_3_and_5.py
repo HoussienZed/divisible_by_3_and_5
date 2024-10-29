7ddae0722c9279b16d117296d88432438b2e17ce
Houssien Zeineddine <houessienzd@hotmail.com>
2024-10-29 20:47:42 +0200

    first commit

---
+++ divisible_by_3_and_5.py
@@ -0,0 +1,11 @@
+# prompting user to enter a number x
+
+x = int(input("Enter an integer: "))
+
+# checking what numbers from 1 to x divisible by 3 and 5
+
+for i in range(1, x + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print(f"{i} is divisible by 3 and 5")
+else:
+    print(f"There are no integers between 1 and {x} divisible by 3 and 5")
